0c7c995908de9c22478c7f18e6c603ef8275da03
Akshansh0812 <akshanshanand2001@gmail.com>
2023-03-11 12:21:08 +0530

    Include BMI index and valid inputs function

--- app/src/main/java/com/example/bmicalulator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalulator/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +25,30 @@ class MainActivity : AppCompatActivity() {
             val height = heightText.text.toString()
 
             // calculating bmi
-            val bmi = weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
-            val bmi2Digits = String.format("%.2f",bmi).toFloat()
-            displayResult(bmi2Digits)
+            if(validateInput(weight,height)) {
+                val bmi = weight.toFloat() / ((height.toFloat() / 100) * (height.toFloat() / 100))
+                val bmi2Digits = String.format("%.2f", bmi).toFloat()
+                displayResult(bmi2Digits)
+            }
         }
     }
+    // if user does not provide inputs
+    private fun validateInput(weight:String? , height:String?):Boolean{
+        return when{
+            weight.isNullOrEmpty() ->{
+                Toast.makeText(this,"Weight is empty", Toast.LENGTH_LONG).show()
+                return false
+            }
+            height.isNullOrEmpty() ->{
+                Toast.makeText(this,"Height is empty", Toast.LENGTH_LONG).show()
+                return false
+            }
+            else ->{
+                return true
+            }
+        }
+    }
+
     private fun displayResult(bmi:Float){
         val resultIndex = findViewById<TextView>(R.id.tvIndex)
         val resultDescription = findViewById<TextView>(R.id.tvResult)
@@ -35,6 +56,31 @@ class MainActivity : AppCompatActivity() {
 
         resultIndex.text = bmi.toString()
         info.text = "(Normal range is 18.5 to 24.9)"
+
+        var resultText = ""
+        var color = 0
+
+        when {
+            bmi<18.50 ->{
+                resultText = "Underweight"
+                color = R.color.under_weight
+            }
+            bmi in 18.50..24.99 ->{
+                resultText = "Healthy"
+                color = R.color.normal
+            }
+            bmi in 25.00..29.99 ->{
+                resultText = "Overweight"
+                color = R.color.over_weight
+            }
+            bmi>29.99 ->{
+                resultText = "Obese"
+                color = R.color.obese
+            }
+        }
+        // to change the color of the textview
+        resultDescription.setTextColor(ContextCompat.getColor(this,color))
+        resultDescription.text = resultText
     }
 }
 // designing part
